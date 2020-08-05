341adb253964d521fe85bc28b9b6f33842d17454
yoyomodesign <52845357+yoyomodesign@users.noreply.github.com>
2020-08-04 18:53:36 -0700

    Updates

---
+++ precache-manifest.18cfbab7260a7faff63e3a2ca5ec42c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3591a2018750a6b76f41f3fbe0de08",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "5830072dc3d46af7228a",
+    "url": "/weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b1b72ef4f74d73f221a",
+    "url": "/weather/static/js/2.f3f0c671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.f3f0c671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5830072dc3d46af7228a",
+    "url": "/weather/static/js/main.c8e5451c.chunk.js"
+  },
+  {
+    "revision": "e2424e2c91690110d37f",
+    "url": "/weather/static/js/runtime-main.8ca140f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//weather/precache-manifest.fe7370badd98487db8114a291bae748c.js"
+  "/weather/precache-manifest.18cfbab7260a7faff63e3a2ca5ec42c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
